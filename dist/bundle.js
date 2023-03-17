ed224ab6bba6968f403e17d6af5fa743ebb46767
高试科 <2832491856@qq.com>
2023-03-17 20:07:41 +0800

    继续研究 引入模块

--- dist/bundle.js
+++ dist/bundle.js
@@ -1,2 +1,4 @@
+import { home } from './index';
 console.log('我是傻逼');
 console.log('我是傻逼1');
+console.log('我是傻逼3',home);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+import { home } from './index';
+
 console.log('我是傻逼');
 console.log('我是傻逼1');
-console.log('我是傻逼3');
+console.log('我是傻逼3',home);
 
